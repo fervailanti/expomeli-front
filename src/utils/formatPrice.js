b63f25ef67e1fad041394160779713585d579fe4
fervailanti <fernando.vailanti@iunigo.com>
2020-10-18 07:38:01 -0300

    edit price parser for CI

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -3,7 +3,7 @@ import { CURRENCIES } from './constants'
 export const formatPrice = (price) => {
   const { currency, amount, decimals } = price
   const formattedCurrency = CURRENCIES[currency] || CURRENCIES.default
-  const formattedAmount = Intl.NumberFormat('de-DE').format(amount)
+  const formattedAmount = amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
   const fixedPrice = decimals ? `${formattedAmount},${decimals}` : formattedAmount
   return `${formattedCurrency} ${fixedPrice}`
 }
