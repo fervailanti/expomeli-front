5c20f55d3d8f5ddd4a8f5a289ceeb0bc596580ec
fervailanti <fernando.vailanti@iunigo.com>
2020-10-18 07:08:39 -0300

    wip tests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,5 @@
 export const API_BASE_URL = 'https://expomeli.herokuapp.com/api'
 
-export const PRICE_FORMAT = 'de-DE'
-
 export const CONDITIONS = {
   new: 'Nuevo',
   used: 'Usado',
--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -1,9 +1,9 @@
-import { CURRENCIES, PRICE_FORMAT } from './constants'
+import { CURRENCIES } from './constants'
 
 export const formatPrice = (price) => {
   const { currency, amount, decimals } = price
   const formattedCurrency = CURRENCIES[currency] || CURRENCIES.default
-  const formattedAmount = Intl.NumberFormat(PRICE_FORMAT).format(amount)
+  const formattedAmount = Intl.NumberFormat('de-DE').format(amount)
   const fixedPrice = decimals ? `${formattedAmount},${decimals}` : formattedAmount
   return `${formattedCurrency} ${fixedPrice}`
 }
